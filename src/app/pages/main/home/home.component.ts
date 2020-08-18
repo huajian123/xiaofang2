a5a9838c6323f4527b68b9ff2b9c41d0e4e08a29
huajian <287643967@qq.com>
2020-08-18 09:43:36 +0800

    初次提交

--- src/app/pages/main/home/home.component.ts
+++ src/app/pages/main/home/home.component.ts
@@ -1,4 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {PublishAlarmModel} from "../../../services/biz-services/earthquake-warning-list.service";
+import {UserRole} from "../../../VO/types";
+import {MapPipe, MapSet} from "../../../share/directives/pipe/map.pipe";
+export enum VariableEnum {
+  zero = 0,
+  one = 1,
+  two = 2,
+  three = 3,
+  four = 4,
+  twenty = 20
+}
+
+interface OptionsInterface {
+  value: number;
+  label: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,10 +22,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-
+  selAlarm: PublishAlarmModel;
+  userRole: number;
+  userRoleEnum = UserRole;
+  currentPage: number;
+  accidentType: number;
+  accidentId: number;
+  numVariable = VariableEnum;
+  /*中间数组变量*/
+  temporaryNameOptions: OptionsInterface[];
+  /*灾害类型下拉*/
+  accidentTypeOptions: OptionsInterface[];
+  /*自然灾害名称下拉*/
+  naturalNameOptions: OptionsInterface[];
+  /*事故灾害名称下拉*/
+  accidentNameOptions: OptionsInterface[];
+  /*公共卫生下拉*/
+  publicHealthNameOptions: OptionsInterface[];
+  /*社会安全下拉*/
+  socialSecurityNameOptions: OptionsInterface[];
   constructor() { }
 
+
+  changeAccidentIdValue(index) {
+    this.temporaryNameOptions = [];
+    if (index !== null) {
+      switch (index) {
+        case 1:
+          this.temporaryNameOptions = this.naturalNameOptions;
+          break;
+        case 2:
+          this.temporaryNameOptions = this.accidentNameOptions;
+          break;
+        case 3:
+          this.temporaryNameOptions = this.publicHealthNameOptions;
+          break;
+        default:
+          this.temporaryNameOptions = this.socialSecurityNameOptions;
+          break;
+      }
+      this.accidentId = null;
+    }
+  }
+
+  goRest() {
+    this.accidentType = null;
+    this.accidentId = null;
+    this.currentPage = 0;
+  }
+
+  goSure() {
+    if (this.accidentId === null || this.accidentType === null) {
+      return;
+    }
+    this.currentPage = this.accidentId;
+  }
+
   ngOnInit(): void {
+    this.accidentTypeOptions = [...MapPipe.transformMapToArray(MapSet.accidentType)];
+    this.naturalNameOptions = [...MapPipe.transformMapToArray(MapSet.naturalDisastersType)];
+    this.accidentNameOptions = [...MapPipe.transformMapToArray(MapSet.accidentDisastersType)];
+    this.publicHealthNameOptions = [...MapPipe.transformMapToArray(MapSet.publicHealthType)];
+    this.socialSecurityNameOptions = [...MapPipe.transformMapToArray(MapSet.socialSecurityType)];
   }
 
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,11 +4,12 @@ import {MyNgZorroModule} from '../share/my-ng-zorro/my-ng-zorro.module';
 import {LoginLayoutComponent} from "./login/login-layout/login-layout.component";
 import {ShareModule} from "../share/share.module";
 import { HomeComponent } from './main/home/home.component';
+import { HazardousComponent } from './main/home/hazardous/hazardous.component';
 
 
 
 @NgModule({
-  declarations: [LoginLayoutComponent, HomeComponent],
+  declarations: [LoginLayoutComponent, HomeComponent, HazardousComponent],
   imports: [
     CommonModule,
     ShareModule,
