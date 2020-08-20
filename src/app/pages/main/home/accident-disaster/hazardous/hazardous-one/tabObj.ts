5970bf09b8901b23d2dfca4f74ed3011d5c1d81c
huajian <287643967@qq.com>
2020-08-20 10:27:45 +0800

    代码修改

--- src/app/pages/main/home/accident-disaster/hazardous/hazardous-one/tabObj.ts
+++ src/app/pages/main/home/accident-disaster/hazardous/hazardous-one/tabObj.ts
@@ -1,8 +1,8 @@
 export function getHazardousTabObj(leftDivName) {
-  if(leftDivName==="启动应急响应"){
+  if (leftDivName === '启动应急响应') {
     return [
       {
-        name: "启动应急响应", id: 1,
+        name: '启动应急响应', id: 1,
         children: [
           {name: '启动应急响应111', id: 13},
           {
@@ -13,22 +13,22 @@ export function getHazardousTabObj(leftDivName) {
             ]
           }]
       },
-      {name: "应急管理厅1", id: 2},
-      {name: "应急管理厅2", id: 3},
-      {name: "应急管理厅3", id: 4},
-      {name: "应急管理厅4", id: 5},
-      {name: "应急管理厅5", id: 6},
-      {name: "应急管理厅6", id: 7},
-      {name: "应急管理厅7", id: 8},
-      {name: "应急管理厅8", id: 9},
-      {name: "应急管理厅9", id: 10},
-      {name: "应急管理厅10", id: 11},
+      {name: '应急管理厅1', id: 2},
+      {name: '应急管理厅2', id: 3},
+      {name: '应急管理厅3', id: 4},
+      {name: '应急管理厅4', id: 5},
+      {name: '应急管理厅5', id: 6},
+      {name: '应急管理厅6', id: 7},
+      {name: '应急管理厅7', id: 8},
+      {name: '应急管理厅8', id: 9},
+      {name: '应急管理厅9', id: 10},
+      {name: '应急管理厅10', id: 11},
     ]
   }
-  if(leftDivName==="善后处理与事故调查"){
+  if (leftDivName === '善后处理与事故调查') {
     return [
       {
-        name: "保障方案11", id: 12,
+        name: '保障方案11', id: 12,
         children: [
           {name: '保障方案', id: 17},
           {
