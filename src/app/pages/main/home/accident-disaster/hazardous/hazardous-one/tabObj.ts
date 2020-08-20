d2874395915374fe4030d3e1f0c9c9d148104b13
huajian <287643967@qq.com>
2020-08-20 10:27:10 +0800

    代码修改

--- src/app/pages/main/home/accident-disaster/hazardous/hazardous-one/tabObj.ts
+++ src/app/pages/main/home/accident-disaster/hazardous/hazardous-one/tabObj.ts
@@ -4,12 +4,12 @@ export function getHazardousTabObj(leftDivName) {
       {
         name: "启动应急响应", id: 1,
         children: [
-          {name: '启动应急响应111', id: 18},
+          {name: '启动应急响应111', id: 13},
           {
-            name: '启动应急响应', id: 8,
+            name: '启动应急响应', id: 14,
             children: [
-              {name: '启动应急响应', id: 9},
-              {name: '启动应急响应', id: 10},
+              {name: '启动应急响应', id: 15},
+              {name: '启动应急响应', id: 16},
             ]
           }]
       },
@@ -18,24 +18,24 @@ export function getHazardousTabObj(leftDivName) {
       {name: "应急管理厅3", id: 4},
       {name: "应急管理厅4", id: 5},
       {name: "应急管理厅5", id: 6},
-      {name: "应急管理厅5", id: 11},
-      {name: "应急管理厅5", id: 12},
-      {name: "应急管理厅5", id: 13},
-      {name: "应急管理厅5", id: 14},
-      {name: "应急管理厅5", id: 15},
+      {name: "应急管理厅6", id: 7},
+      {name: "应急管理厅7", id: 8},
+      {name: "应急管理厅8", id: 9},
+      {name: "应急管理厅9", id: 10},
+      {name: "应急管理厅10", id: 11},
     ]
   }
   if(leftDivName==="善后处理与事故调查"){
     return [
       {
-        name: "保障方案", id: 1,
+        name: "保障方案11", id: 12,
         children: [
-          {name: '保障方案', id: 7},
+          {name: '保障方案', id: 17},
           {
-            name: '保障方案', id: 8,
+            name: '保障方案', id: 18,
             children: [
-              {name: '保障方案', id: 9},
-              {name: '保障方案', id: 10},
+              {name: '保障方案', id: 19},
+              {name: '保障方案', id: 20},
             ]
           }]
       },
--- src/app/share/biz-component/biz-tabs/biz-tabs.component.ts
+++ src/app/share/biz-component/biz-tabs/biz-tabs.component.ts
@@ -1,4 +1,14 @@
-import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewEncapsulation
+} from '@angular/core';
 
 export interface TabObjModel {
   name: string;
@@ -12,7 +22,7 @@ export interface TabObjModel {
   styleUrls: ['./biz-tabs.component.less'],
   encapsulation: ViewEncapsulation.None
 })
-export class BizTabsComponent implements OnInit {
+export class BizTabsComponent implements OnInit, OnChanges {
   @Input() tabObj: TabObjModel[];
   @Input() currentSelFirstTabId: number;
   @Input() currentSelSecondTabId: number;
@@ -21,7 +31,7 @@ export class BizTabsComponent implements OnInit {
   thirdLevelTabArray = [];
   @Output() selId: EventEmitter<number>;
 
-  constructor() {
+  constructor(private cdr: ChangeDetectorRef) {
     this.selId = new EventEmitter<number>();
   }
 
@@ -33,24 +43,54 @@ export class BizTabsComponent implements OnInit {
     }
   }
 
-  chooseTab(tabItem, level) {
+  chooseTab(tabItem: TabObjModel, level) {
     if (level === 1) {
       this.currentSelFirstTabId = tabItem.id;
-      this.secondLevelTabArray = tabItem.children || [];
+      if (tabItem.children) {
+        this.currentSelSecondTabId = tabItem.children[0].id
+        // 存放2级tab数组
+        this.secondLevelTabArray = tabItem.children
+        if (tabItem.children[0].children) {
+          this.currentSelThirdTabId = tabItem.children[0].children[0].id;
+          // 存放3级tab数组
+          this.thirdLevelTabArray = tabItem.children[0].children;
+        } else {
+          this.currentSelThirdTabId = null;
+          this.thirdLevelTabArray = [];
+        }
+      } else {
+        this.currentSelSecondTabId = null;
+        this.currentSelThirdTabId = null;
+        this.thirdLevelTabArray = [];
+        this.secondLevelTabArray = [];
+      }
     } else if (level === 2) {
       this.currentSelSecondTabId = tabItem.id;
-      this.thirdLevelTabArray = tabItem.children || [];
+      if (tabItem.children) {
+        this.currentSelThirdTabId = tabItem.children[0].id
+        this.thirdLevelTabArray = tabItem.children
+      } else {
+        this.thirdLevelTabArray = [];
+        this.currentSelThirdTabId = null;
+      }
     } else {
       this.currentSelThirdTabId = tabItem.id;
     }
     const selId = this.findId(tabItem)
-    console.log(this.tabObj);
-    console.log(selId);
-    this.selId.emit(tabItem.id);
+    this.selId.emit(selId);
   }
 
   ngOnInit(): void {
     this.secondLevelTabArray = this.tabObj[0]?.children || [];
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!changes['tabObj'].firstChange) {
+      this.tabObj = changes['tabObj'].currentValue;
+      this.chooseTab(this.tabObj[0], 1)
+      this.cdr.markForCheck();
+    }
+
   }
 
 }
