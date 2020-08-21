145d816ac80ac4fb097b1a64e4a40705ed8e91b8
sunxulong4399 <1015435340@qq.com>
2020-08-21 10:15:26 +0800

    3、4级危化品修改

--- src/app/pages/main/home/accident-disaster/hazardous/hazardous-three/hazardous-three.component.ts
+++ src/app/pages/main/home/accident-disaster/hazardous/hazardous-three/hazardous-three.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {DataShowModel} from '../../../../../../share/biz-component/data-show/data-show.component';
 import {LoginInfoModel} from '../../../../../../core/vo-common/BusinessEnum';
 import {TabObjModel} from '../../../../../../share/biz-component/biz-tabs/biz-tabs.component';
 import {UserRole} from '../../../../../../VO/types';
 import {EVENT_KEY} from '../../../../../../../environments/staticVariable';
+import {CitiesNameService, ResponsibilityEntitiesModel} from '../../../../../../services/biz-services/earthquake-warning-list.service';
+import {getHazardousThreeTabObj} from './hazardous-three-tab-obj';
 
 @Component({
   selector: 'app-hazardous-three',
@@ -12,75 +13,59 @@ import {EVENT_KEY} from '../../../../../../../environments/staticVariable';
 })
 export class HazardousThreeComponent implements OnInit {
   loginInfo: LoginInfoModel;
+  firstSelTabOneLevelId: number;
+  firstSelTabTwoLevelId: number;
+  firstSelTabThreeLevelId: number;
   nameArray: string[];
-  showInfo: DataShowModel = {
-    linkPhone: '1131313131',
-    linkPeople: '张三',
-    duty: ['先做第一步', '再做第二部']
-  };
+  showInfo: ResponsibilityEntitiesModel;
   tabObj: TabObjModel[];
+  cityName: string;
 
-  constructor() {
+  constructor(private dataService: CitiesNameService) {
+    this.cityName = '';
+    this.showInfo = {
+      linkman: '',
+      linkPhone: '',
+      responsibilityDetailSort: []
+    };
     this.nameArray = [];
     this.tabObj = [];
+    this.firstSelTabOneLevelId = null;
+    this.firstSelTabTwoLevelId = null;
+    this.firstSelTabThreeLevelId = null;
   }
 
   // 点击左侧六边形获取当前名字
   getCurrentLeftName(event) {
-    if (event === '启动应急响应') {
-      this.tabObj = [
-        {
-          name: '应急管理厅1', id: 1,
-          children: [
-            {name: '救灾处1', id: 7},
-            {
-              name: '安平处1', id: 8,
-              children: [
-                {name: '应急管理厅1', id: 9},
-                {name: '应急管理厅1', id: 10},
-              ]
-            }]
-        },
-        {name: '应急管理厅1', id: 2},
-        {name: '应急管理厅2', id: 3},
-        {name: '应急管理厅3', id: 4},
-        {name: '应急管理厅4', id: 5},
-        {name: '应急管理厅5', id: 6},
-        {name: '应急管理厅5', id: 11},
-        {name: '应急管理厅5', id: 12},
-        {name: '应急管理厅5', id: 13},
-        {name: '应急管理厅5', id: 14},
-        {name: '应急管理厅5', id: 15},
-      ];
-    } else if (event === '保障方案') {
-      this.tabObj = [
-        {
-          name: '应急管理厅1', id: 1,
-          children: [
-            {name: '救灾处1', id: 7},
-            {
-              name: '安平处1', id: 8,
-              children: [
-                {name: '应急管理厅1', id: 9},
-                {name: '应急管理厅1', id: 10},
-              ]
-            }]
-        },
-      ];
+    this.tabObj = getHazardousThreeTabObj(event);
+    this.firstSelTabOneLevelId = this.tabObj[0].id;
+    if (this.tabObj[0].children) {
+      this.firstSelTabTwoLevelId = this.tabObj[0].children[0].id;
+      if (this.tabObj[0].children[0].children) {
+        this.firstSelTabThreeLevelId = this.tabObj[0].children[0].children[0].id;
+      } else {
+        this.firstSelTabThreeLevelId = null;
+      }
+    } else {
+      this.firstSelTabTwoLevelId = null;
+      this.firstSelTabThreeLevelId = null;
     }
-    console.log('当前点击的六边形的名字是：' + event);
   }
 
   // 获取当前选中的tabid
   getSelId(event) {
-    console.log('当前点击的tab的id是：' + event);
+    this.dataService.getGroupInfo({id: event, cityName: this.cityName}).subscribe(res => {
+      this.showInfo = res.responsibilityEntities;
+      /*console.log(this.showInfo);*/
+    });
   }
 
   judgRole() {
+    this.nameArray = ['事故信息接收', '应急指导'];
     if (this.loginInfo.role === UserRole.User) {
-      this.nameArray = ['善后处理<br/>与事故调查'];
+      /* this.nameArray = ['善后处理<br/>与事故调查'];*/
     } else if (this.loginInfo.role === UserRole.Manage) {
-      this.nameArray = ['启动应急响应', '分组开展<br/>应急救援工作', '保障方案', '现场信息采集', '辅助指挥<br/>决策信息', '事态控制', '善后处理<br/>与事故调查'];
+      /* this.nameArray = ['事故信息接收', '应急指导'];*/
     }
   }
 
