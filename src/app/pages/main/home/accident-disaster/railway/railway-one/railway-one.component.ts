a552f48d3bc3e6586b675dfc770decaec93f511d
huajian <287643967@qq.com>
2020-11-26 10:42:15 +0800

    添加修改铁路突发事故的指令手册bug

--- src/app/pages/main/home/accident-disaster/railway/railway-one/railway-one.component.ts
+++ src/app/pages/main/home/accident-disaster/railway/railway-one/railway-one.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {bounceInOnEnterAnimation, lightSpeedInOnEnterAnimation} from 'angular-animations';
-import {ResponsibilityModel} from '../../../../../../services/biz-services/accident-disasters-list.service';
+import {EmergencyModel, ResponsibilityModel} from '../../../../../../services/biz-services/accident-disasters-list.service';
 import {CitiesNameService} from '../../../../../../services/biz-services/earthquake-warning-list.service';
 
 @Component({
@@ -18,13 +18,17 @@ export class RailwayOneComponent implements OnInit {
   @Input() currentPage: number;
   @Input() planId: number;
   @Input() downLoadUrl: string;
+  @Input() emergencyRoomData: EmergencyModel[];
   nameArray: string[];
   tableObj: ResponsibilityModel[];
   level: number;
   levels: number;
+  emergencyObj: EmergencyModel[];
+
   constructor(private dataService: CitiesNameService) {
     this.nameArray = [];
     this.tableObj = [];
+    this.emergencyObj = [];
   }
 
   // 点击左侧六边形获取当前名字
@@ -37,6 +41,7 @@ export class RailwayOneComponent implements OnInit {
     this.level = this.currentPage;
     this.levels = this.planId;
     this.tableObj = this.responsibilityData;
+    this.emergencyObj = this.emergencyRoomData;
   }
 
 }
