03cef26d18f3dd98427c83a5fc29f6041b1b7be1
huajian <287643967@qq.com>
2020-11-30 09:25:23 +0800

    范宝林提出问题修改

--- src/app/pages/main/home/accident-disaster/major-fire/major-fire-three/major-fire-three.component.ts
+++ src/app/pages/main/home/accident-disaster/major-fire/major-fire-three/major-fire-three.component.ts
@@ -1,9 +1,14 @@
 import {Component, OnInit} from '@angular/core';
+import {bounceInOnEnterAnimation, lightSpeedInOnEnterAnimation} from 'angular-animations';
 
 @Component({
   selector: 'app-major-fire-three',
   templateUrl: './major-fire-three.component.html',
-  styleUrls: ['./major-fire-three.component.less']
+  styleUrls: ['./major-fire-three.component.less'],
+  animations: [
+    lightSpeedInOnEnterAnimation(),
+    bounceInOnEnterAnimation(),
+  ]
 })
 export class MajorFireThreeComponent implements OnInit {
 
