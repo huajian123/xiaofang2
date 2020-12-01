67bc57cd4d3cecc041d09826989fddf0746ada25
sunxulong4399 <1015435340@qq.com>
2020-12-01 09:58:06 +0800

    气象灾害三四级错误修改

--- src/app/pages/main/home/natural-disaster/meteorological/meteorological-three/meteorological-three.component.ts
+++ src/app/pages/main/home/natural-disaster/meteorological/meteorological-three/meteorological-three.component.ts
@@ -1,9 +1,14 @@
 import {Component, OnInit} from '@angular/core';
+import {bounceInOnEnterAnimation, lightSpeedInOnEnterAnimation} from 'angular-animations';
 
 @Component({
   selector: 'app-meteorological-three',
   templateUrl: './meteorological-three.component.html',
-  styleUrls: ['./meteorological-three.component.less']
+  styleUrls: ['./meteorological-three.component.less'],
+  animations: [
+    lightSpeedInOnEnterAnimation(),
+    bounceInOnEnterAnimation(),
+  ]
 })
 export class MeteorologicalThreeComponent implements OnInit {
 
